70f0fa8d7640b60faddeafa4b3a29a0627f25aa4
juanfeng <zhangzhujuan@ZHANGiMac.local>
2015-04-15 13:31:04 +0800

    helloword
    helloword

---
+++ helloWorld/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  helloWorld
+//
+//  Created by zzj on 14/12/30.
+//  Copyright (c) 2014年 zzj. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
